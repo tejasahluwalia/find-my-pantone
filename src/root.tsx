5a5466daa9f0fab67a85462321799bf4025734d2
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2023-05-13 07:28:53 +0530

    Initial commit

---
+++ src/root.tsx
@@ -0,0 +1,53 @@
+// @refresh reload
+import { Suspense } from "solid-js";
+import {
+	useLocation,
+	A,
+	Body,
+	ErrorBoundary,
+	FileRoutes,
+	Head,
+	Html,
+	Meta,
+	Routes,
+	Scripts,
+	Title,
+} from "solid-start";
+import "./root.css";
+
+export default function Root() {
+	const location = useLocation();
+	const active = (path: string) =>
+		path == location.pathname
+			? "border-sky-600"
+			: "border-transparent hover:border-sky-600";
+	return (
+		<Html lang="en">
+			<Head>
+				<Title>Find my Pantone | Hex to Pantone TCX Converter</Title>
+				<Meta charset="utf-8" />
+				<Meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta
+					name="description"
+					content="Find the closest Pantone TCX colors for any hex color code."
+				/>
+			</Head>
+			<Body>
+				<Suspense>
+					<ErrorBoundary>
+						<Routes>
+							<FileRoutes />
+						</Routes>
+					</ErrorBoundary>
+				</Suspense>
+				<Scripts />
+				<footer class="text-center text-gray-700 p-4">
+					<p class="my-4">
+						PANTONE® and other Pantone trademarks are the property of Pantone
+						LLC.
+					</p>
+				</footer>
+			</Body>
+		</Html>
+	);
+}
