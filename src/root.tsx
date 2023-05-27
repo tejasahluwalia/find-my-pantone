72a19fe82149a36cbbff2b1cedbec5d98974a4f8
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2023-05-28 04:31:14 +0530

    feat: add favicon images

--- src/root.tsx
+++ src/root.tsx
@@ -12,6 +12,7 @@ import {
 	Routes,
 	Scripts,
 	Title,
+	Link,
 } from "solid-start";
 import "./root.css";
 
@@ -35,6 +36,27 @@ export default function Root() {
 					name="google-site-verification"
 					content="PylHrwBjrOCS_OfF-zHvdOxMGMCZzGulDMC9z18CSZg"
 				/>
+				<Link
+					rel="apple-touch-icon"
+					sizes="76x76"
+					href="/apple-touch-icon.png"
+				/>
+				<Link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<Link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<Link rel="manifest" href="/site.webmanifest" />
+				<Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+				<Meta name="msapplication-TileColor" content="#da532c" />
+				<Meta name="theme-color" content="#ffffff"></Meta>
 				<script
 					defer
 					src="https://static.cloudflareinsights.com/beacon.min.js"
